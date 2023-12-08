ab04d270ec0aac898edc8b38f78ddd6ccd3b465a
royal8341 <153269600+royal8341@users.noreply.github.com>
2023-12-08 13:53:07 +0530

    Add files via upload

---
+++ Average of 3 no.s
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  float avg;
+  int sum = a+b+c;
+  avg = sum / 3;
+  cout<<"The average of three numbers = "<<avg;
+  return 0;
+}
---
+++ Compare 2 no.s
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a == b){
+    cout <<a<<" and "<<b<< " are equal";
+  }
+  else{
+    cout <<a<<" and "<<b<< " are not equal";
+  }
+}
